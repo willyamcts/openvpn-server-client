3edaf9cd7c0d36cb278929da3a58d0fd3909f836
Willyam Castro <willyam.android@gmail.com>
2024-10-10 21:07:00 -0300

    start install

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Guides:
+# - https://github.com/OpenVPN/openvpn
+# - https://community.openvpn.net/openvpn/wiki/Openvpn24ManPage
+
+OPENVPN_VERSION=2.6.12
+CONFIG_DIR=/etc/openvpn
+
+cd /usr/src
+
+# requeriments
+apt update
+apt install gcc pkg-config make \
+ libnl-genl-3-dev libcap-ng-dev libssl-dev \
+ liblzo2-dev liblz4-dev \
+ libpam0g-dev -y
+
+# download openvpn version $OPENVPN_VERSION
+wget https://github.com/OpenVPN/openvpn/releases/download/v${OPENVPN_VERSION}/openvpn-${OPENVPN_VERSION}.tar.gz
+tar -xf openvpn-${OPENVPN_VERSION}.tar.gz
+cd openvpn-${OPENVPN_VERSION}
+
+./configure --sysconfdir=$CONFIG_DIR
+make -j$(nproc)
+make install #DESTDIR=/usr/src/openvpn
