28c5b5dc0158241a392aedc4daf9dc9581b070dd
Willyam Castro <willyam.android@gmail.com>
2024-10-20 16:58:39 -0300

    docker container first test - config file to be reviewed in the future

--- docker/install.sh
+++ docker/install.sh
@@ -12,10 +12,10 @@ CERT_DIR=${SERVER_DIR}/cert
 
 # requeriments
 apt update
-apt install gcc pkg-config make \
+apt install -y wget gcc pkg-config make \
  libnl-genl-3-dev libcap-ng-dev libssl-dev \
  liblzo2-dev liblz4-dev \
- libpam0g-dev -y
+ libpam0g-dev
 
 # download openvpn version $OPENVPN_VERSION
 cd /usr/src
@@ -26,8 +26,8 @@ cd openvpn-${OPENVPN_VERSION}
 
 
 # compile/install
-mkdir -p $SERVER_DIR
-./configure --sysconfdir=$SERVER_DIR
+mkdir -p $SERVER_DIR $CERT_DIR
+./configure --prefix=$CONFIG_DIR --sysconfdir=$SERVER_DIR
 make -j$(nproc)
 make install #DESTDIR=/usr/src/openvpn
 
@@ -35,7 +35,7 @@ make install #DESTDIR=/usr/src/openvpn
 echo 'IyBiYXNpYyBzZXR1cAptb2RlIHNlcnZlcgojbG9jYWwgMjgwNDo6CiNsb2NhbCAxOTIuMTY4Li4u
 CnBvcnQgMTE5NApwcm90byB1ZHA2CmRldiB0dW4KCiMgcHJvY2VzcyBydW4KdXNlciBub2JvZHkK
 Z3JvdXAgbm9ncm91cAoKIyBsb2cKI2xvZy1hcHBlbmQgL3Zhci9sb2cvb3BlbnZwbi5sb2cKdmVy
-YiAzCgojIGNlcnRpZmljYXRlCmNhIGNlcnQvY2EuY3J0CiNjZXJ0LmNydApjZXJ0IGNlcnQvc2Vy
+YiA3CgojIGNlcnRpZmljYXRlCmNhIGNlcnQvY2EuY3J0CiNjZXJ0LmNydApjZXJ0IGNlcnQvc2Vy
 dmVyLmNydAojIFRoaXMgZmlsZSBzaG91bGQgYmUga2VwdCBzZWNyZXQKa2V5IGNlcnQvc2VydmVy
 LmtleQoKZGggbm9uZQoKIyBuZXR3b3JrIHByaXZhdGUgVlBOCnRvcG9sb2d5IHN1Ym5ldApzZXJ2
 ZXIgMTAuOC4wLjAgMjU1LjI1NS4yNTUuMApzZXJ2ZXItaXB2NiBmZDQyOjQyOjQyOjQyOjovMTEy
@@ -43,11 +43,10 @@ CgojIGFkZHJlc3MgcmVsZWFzZSBsb2cKaWZjb25maWctcG9vbC1wZXJzaXN0IC92YXIvbG9nL29w
 ZW52cG4vcmVsZWFzZXMudHh0CgojIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjCiMgQ2xpZW50IGFm
 ZmVjdApwZXJzaXN0LWtleQpwZXJzaXN0LXR1bgpwdXNoIHR1bi1pcHY2CgojIEF1dGggd2l0aCB1
 c2VyIGFuZCBwYXNzd29yZApzY3JpcHQtc2VjdXJpdHkgMgphdXRoLXVzZXItcGFzcy12ZXJpZnkg
-L2V0Yy9vcGVudnBuL2NoZWNrcGFzc3dkLnNoIHZpYS1maWxlCnZlcmlmeS1jbGllbnQtY2VydCBv
-cHRpb25hbAojdXNlcm5hbWUtYXMtY29tbW9uLW5hbWUKCiNjbGllbnQtY29uZmlnLWRpciAvZXRj
-L29wZW52cG4vY2NkCiNjY2QtZXhjbHVzaXZlCgojIyBzZXQgcm91dGUKcHVzaCAicm91dGUgMTky
-LjE2OC4wLjAgMjU1LjI1NS4yNTUuMCIKIyMgc2V0IEROUyBsb2NhbApwdXNoICJkaGNwLW9wdGlv
-biBETlMgMS4xLjEuMSIK' |base64 -d > $SERVER_DIR/server.conf
+Y2hlY2twYXNzd2Quc2ggdmlhLWZpbGUKdmVyaWZ5LWNsaWVudC1jZXJ0IG9wdGlvbmFsCiN1c2Vy
+bmFtZS1hcy1jb21tb24tbmFtZQoKI2NsaWVudC1jb25maWctZGlyIC9ldGMvb3BlbnZwbi9jY2QK
+I2NjZC1leGNsdXNpdmUKCiMjIHNldCByb3V0ZQpwdXNoICJyb3V0ZSAxOTIuMTY4LjAuMCAyNTUu
+MjU1LjAuMCIKIyMgc2V0IEROUyBsb2NhbApwdXNoICJkaGNwLW9wdGlvbiBETlMgMS4xLjEuMSIK' |base64 -d > $SERVER_DIR/server.conf
 
 # TODO: Check permissions
 # TODO: Cipher passwords, change to binary maybe
@@ -62,18 +61,18 @@ chmod +x $SERVER_DIR/checkpasswd.sh
 echo 'dXNlcjoxMjMKdXNlcjE6MTIzCg==' |base64 -d > $SERVER_DIR/passwd.file
 
 # Manage certificates
-apt install easy-rsa
+apt install -y easy-rsa
 
 # create certs
-...
+#...
 chmod 640 ${SERVER_DIR}/cert/*.key
-...
+#...
 
 
 sysctl -w net.ipv4.conf.all.forwarding=1 net.ipv6.conf.all.forwarding=1
 
 # running
-/usr/local/sbin/openvpn --cd $SERVER_DIR --config server.conf
+#/usr/local/openvpn/sbin/openvpn --cd $SERVER_DIR --config server.conf
 
 
 
